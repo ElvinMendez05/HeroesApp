87c9e167d4a4c94b84e8baf219800c543aa746ab
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-11-02 17:58:51 -0400

    design updated

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,7 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { SearchControls } from "./ui/SearchControls"
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
 
 export const SearchPage = () => {
   return (
@@ -8,8 +9,17 @@ export const SearchPage = () => {
       <CustomJumbotron title="Search for hero"
         description="Discover, explore, and manage your favorite superheroes and villains" />
       
-      <HeroStats />
+       <CustomBreadcrumbs currentPage="Super Hero" 
+       // breadcrumbs={[
+        //   { label: 'Home1', to: '/' },
+        //   { label: 'Home2', to: '/' },
+        //   { label: 'Home3', to: '/' },
+        // ]}
+       />
 
+      <HeroStats />
+      
+      {/* Filter and search */}
       <SearchControls/>
       </>
       
