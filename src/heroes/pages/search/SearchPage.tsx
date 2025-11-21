5dd7fe086f8b3a08de35a2baeb1dd2454236c6b7
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-11-20 22:19:59 -0400

    filters and updated

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -3,30 +3,39 @@ import { HeroStats } from "@/heroes/components/HeroStats"
 import { SearchControls } from "./ui/SearchControls"
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
 import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { useQuery } from "@tanstack/react-query"
+import { searchHeroesAction } from "@/heroes/actions/search-heros.action"
+import { useSearchParams } from "react-router"
 
 export const SearchPage = () => {
+  
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get('name') ?? undefined;
+   const strength = searchParams.get('strength') ?? undefined;
+
+  const {data: heroes = []} = useQuery({
+      queryKey: ['search', {name, strength}],
+      queryFn: () => searchHeroesAction({name, strength}),
+      staleTime: 1000 * 60 * 5 // 5 minutes
+    }) 
+  
   return (
      <>
       <CustomJumbotron title="Search for hero"
         description="Discover, explore, and manage your favorite superheroes and villains" />
       
-       <CustomBreadcrumbs currentPage="Super Hero" 
-       // breadcrumbs={[
-        //   { label: 'Home1', to: '/' },
-        //   { label: 'Home2', to: '/' },
-        //   { label: 'Home3', to: '/' },
-        // ]}
-       />
+       <CustomBreadcrumbs currentPage="Super Hero"/>
 
       <HeroStats />
       
       {/* Filter and search */}
       <SearchControls/>
       
-      <HeroGrid heroes={[]}/>
+
+      <HeroGrid heroes={heroes}/>
       
       </>
-      
   )
 }
 
